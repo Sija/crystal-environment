ca7c752cedee9065a1b4a44d96da1461160af577
Sijawusz Pur Rahnama <sija@sija.pl>
2017-11-10 03:53:43 +0100

    Refactor `with_env`, just the tiny bit

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,12 +1,12 @@
 require "spec"
 require "../src/crystal-environment"
 
-def with_env(env)
-  old_env = ENV[Crystal::Environment::KEY]?
+def with_env(env : String?) : Void
+  previous_env = ENV[Crystal::Environment::KEY]?
   begin
     ENV[Crystal::Environment::KEY] = env
     yield
   ensure
-    ENV[Crystal::Environment::KEY] = old_env
+    ENV[Crystal::Environment::KEY] = previous_env
   end
 end
