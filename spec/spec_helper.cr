206b69b28ed5a2c0fb9b7009e5e9a6861627a727
Sijawusz Pur Rahnama <sija@sija.pl>
2023-03-06 06:07:38 +0100

    Bump ameba and fix found issues

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,7 +1,7 @@
 require "spec"
 require "../src/crystal-environment"
 
-def with_env(env : String?) : Nil
+def with_env(env : String?, &) : Nil
   previous_env = ENV[Crystal::Environment::KEY]?
   begin
     ENV[Crystal::Environment::KEY] = env
